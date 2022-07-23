3d78656932c1acfc6a013139aeb59be17c455f8c
herdhebayor <98285362+herdhebayor@users.noreply.github.com>
2022-07-23 13:02:08 -0700

    Update rating.js

--- interactive-rating-component-main/rating.js
+++ interactive-rating-component-main/rating.js
@@ -1,29 +1,4 @@
-// const submit = $("#submit")
-// let rating = $(".button")
-// console.log(rating.val())
-// const hide = $("#rating")
-// let input = $(".input span")
-// console.log(input.value)
-// let thanks = $("#thanks")
 
-// rating.on("click", function change(e){
-// $(this).css("background-color", "hsl(25, 97%, 53%)")
-// input = $(this).text()
-// console.log(input)
-// e.preventDefault()
-
-// })
-// submit.on("click", function show(){
-//     if(input === "0"){
-//         alert("please select a rating")
-//     }
-//     else{
-//         thanks.show()
-//         hide.hide()
-//     }
-// })
-
-// WITHOUT JQUERY
 const rating = document.getElementById('rating')
 const thanks = document.getElementById('thanks')
 let text = document.getElementById('data')
@@ -31,15 +6,7 @@ const button =  document.getElementById('button')
  const submit = document.getElementById('submit');
  
    
-//  for(let i = 0; i<button.length; i++){
-//     button[i].addEventListener('click', function click(){
-//       this.style.background = 'hsl(25, 97%, 53%)'
-//       text.innerHTML = button[i].value
-//     });
-//     if(button[1] = click){
-//       click= false
-//     }
-//  }
+
  button.addEventListener('click', e=>{
    const btn = e.target.innerHTML
    text.innerHTML = btn
